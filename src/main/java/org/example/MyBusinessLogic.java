c07fe5414c615a78cf35cb7575e8c2ece1b8dab5
Frederic Mencier <fmencier@yahoo.fr>
2024-03-13 11:34:21 +0100

    work

--- src/main/java/org/example/MyBusinessLogic.java
+++ src/main/java/org/example/MyBusinessLogic.java
@@ -2,8 +2,11 @@ package org.example;
 
 import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 public class MyBusinessLogic {
+
+    private static final Logger LOG = Logger.getLogger(MyBusinessLogic.class.getName());
     private final Boolean bool = new Boolean(true);
 
     private static String bigString = "voici une " +
@@ -19,12 +22,10 @@ public class MyBusinessLogic {
         Float f = new Float(1.1f);
 
         if (!myOption.isPresent()) {
-            System.out.println("mon option est vide");
+            LOG.info("mon option est vide");
         } else {
-            System.out.println(
-                    String.format("mon option contient la valeur %s", myOption.get()));
+            LOG.info(String.format("mon option contient la valeur %s", myOption.get()));
         }
-
     }
 
 

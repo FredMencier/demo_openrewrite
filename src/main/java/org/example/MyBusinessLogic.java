46369d2e7b5855944d2b567ca119b90dc17ed529
Frederic Mencier <fmencier@yahoo.fr>
2024-03-25 16:43:17 +0100

    work

--- src/main/java/org/example/MyBusinessLogic.java
+++ src/main/java/org/example/MyBusinessLogic.java
@@ -1,33 +1,30 @@
 package org.example;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.logging.Logger;
 
 public class MyBusinessLogic {
 
     private static final Logger LOG = Logger.getLogger(MyBusinessLogic.class.getName());
-    private final Boolean bool = Boolean.valueOf(true);
+    private final Boolean bool = new Boolean(true);
 
-    private static String bigString = """
-            voici une \
-            grande phrase \
-            avec des retours \
-            à la ligne \
-            pour plus de clareté.\
-            """;
+    private static String bigString = "voici une " +
+            "grande phrase " +
+            "avec des retours " +
+            "à la ligne " +
+            "pour plus de clareté.";
 
     public void doSomething(Optional<String> myOption) {
-        Byte b = Byte.valueOf("1");
-        Character c = Character.valueOf('c');
-        Double d = Double.valueOf(1.0);
-        Float f = Float.valueOf(1.1f);
+        Byte b = new Byte("1");
+        Character c = new Character('c');
+        Double d = new Double(1.0);
+        Float f = new Float(1.1f);
 
-        if (myOption.isEmpty()) {
+        if (!myOption.isPresent()) {
             LOG.info("mon option est vide");
         } else {
-            LOG.info("mon option contient la valeur %s".formatted(myOption.get()));
+            LOG.info(String.format("mon option contient la valeur %s", myOption.get()));
         }
     }
-
-
 }
